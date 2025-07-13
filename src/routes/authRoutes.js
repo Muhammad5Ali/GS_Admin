0e2274af47889b7dd82ace60244556585f11472a
MohammadAli-14 <rajaaliking789@gmail.com>
2025-07-13 12:22:26 -0700

    Adding routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {register,verifyOTP,resendOTP,login, logout, getUser, forgotPassword,verifyResetOTP, resetPasswordWithOTP} from "../controllers/userController.js"
+import {register,verifyOTP,resendOTP,login, logout, getUser, forgotPassword,verifyResetOTP, resetPasswordWithOTP,registerSupervisor} from "../controllers/userController.js"
 import { isAuthenticated } from "../middleware/auth.js";
 
 const router=express.Router();
 
 router.post("/register",register);
+router.post("/register-supervisor", registerSupervisor);
 router.post("/verifyOTP",verifyOTP);
 router.post("/resendOTP", resendOTP);
 router.post("/login",login);
