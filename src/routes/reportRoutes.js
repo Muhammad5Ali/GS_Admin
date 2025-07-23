50767267978c405ad855745e2aea7e9c28e16af9
MohammadAli-14 <rajaaliking789@gmail.com>
2025-07-23 03:09:40 -0700

    adding the email of supervisor in reportRoutes

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -351,8 +351,8 @@ router.get("/:id", isAuthenticated, async (req, res) => {
   try {
     const report = await Report.findById(req.params.id)
       .populate('user', 'username profileImage')
-      .populate('assignedTo', 'username profileImage')
-      .populate('resolvedBy', 'username profileImage')
+      .populate('assignedTo', 'username email profileImage')
+      .populate('resolvedBy', 'username email profileImage')
       .populate('permanentlyResolvedBy', 'username email profileImage')
       .populate('rejectedBy', 'username email profileImage')
       .populate('outOfScopeBy', 'username email profileImage');
